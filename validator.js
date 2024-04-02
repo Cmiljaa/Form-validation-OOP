c45bc8c1bac91dfa995b8091e239e616bfbec0fb
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-02 16:36:38 +0200

    Version 1

---
+++ validator.js
@@ -0,0 +1,15 @@
+class Validator{
+    constructor(config){
+        this.elementsConfig = config;
+        this.errors = {};
+
+        this.generateErrorsObject();
+    }
+
+    generateErrorsObject() {
+        for(let field in this.elementsConfig)
+        {
+            this.errors[field] = [];
+        }
+    }
+}
